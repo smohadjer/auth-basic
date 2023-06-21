7453fae0779baf5b6fbaf03ca3be68b0d260dc49
Saeid Mohadjer <saeid@fastmail.fm>
2023-06-21 08:36:43 +0200

    fix

--- middleware.js
+++ middleware.js
@@ -1,19 +1,14 @@
-import { rewrite, next } from '@vercel/edge';
+// Basic Auth example via Vercel edge middleware and without using a framework
+
+import { next } from '@vercel/edge';
 
 export default function middleware(req) {
   const url = new URL(req.url);
 
   if (url.pathname.startsWith('/admin')) {
-    //const requestHeaders = new Headers(request.headers);
-    //const auth = requestHeaders.get('Authorization');
     const basicAuth = req.headers.get('authorization');
 
-    if (!basicAuth) {
-      return new Response('credentials missing', {
-        status: 401,
-        headers: { 'WWW-Authenticate': 'Basic' },
-      });
-    } else {
+    if (basicAuth) {
       const authValue = basicAuth.split(' ')[1]
       const [user, pwd] = atob(authValue).split(':')
 
@@ -25,6 +20,11 @@ export default function middleware(req) {
           headers: { 'WWW-Authenticate': 'Basic' },
         });
       }
+    } else {
+      return new Response('credentials missing', {
+        status: 401,
+        headers: { 'WWW-Authenticate': 'Basic' },
+      });
     }
   }
 }
