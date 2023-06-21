c14162204a3701cae58ba41e881cceb7d0521dfc
Saeid Mohadjer <saeid@fastmail.fm>
2023-06-21 10:15:56 +0200

    adds matcher to middleware

--- middleware.js
+++ middleware.js
@@ -5,26 +5,30 @@ import { next } from '@vercel/edge';
 export default function middleware(req) {
   const url = new URL(req.url);
 
-  if (url.pathname.startsWith('/admin')) {
-    const basicAuth = req.headers.get('authorization');
+  console.log('url:', url.pathname);
+  const basicAuth = req.headers.get('authorization');
 
-    if (basicAuth) {
-      const authValue = basicAuth.split(' ')[1]
-      const [user, pwd] = atob(authValue).split(':')
+  if (basicAuth) {
+    const authValue = basicAuth.split(' ')[1]
+    const [user, pwd] = atob(authValue).split(':')
 
-      if (user === 'admin' && pwd === 'admin') {
-        next();
-      } else {
-        return new Response('credentials wrong', {
-          status: 401,
-          headers: { 'WWW-Authenticate': 'Basic' },
-        });
-      }
+    if (user === 'admin' && pwd === 'admin') {
+      next();
     } else {
-      return new Response('credentials missing', {
+      return new Response('credentials wrong', {
         status: 401,
         headers: { 'WWW-Authenticate': 'Basic' },
       });
     }
+  } else {
+    return new Response('credentials missing', {
+      status: 401,
+      headers: { 'WWW-Authenticate': 'Basic' },
+    });
   }
 }
+
+export const config = {
+  // Only run the middleware on the home route
+  matcher: '/admin',
+};
